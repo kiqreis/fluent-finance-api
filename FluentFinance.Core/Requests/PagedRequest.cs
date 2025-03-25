c2a5619413934876a2366a8ba9eb04bbf46310fb
Kaique Reis <kreis1265@gmail.com>
2025-03-25 01:36:47 -0300

    feat: standardizing response

--- FluentFinance.Core/Requests/PagedRequest.cs
+++ FluentFinance.Core/Requests/PagedRequest.cs
@@ -2,6 +2,6 @@ namespace FluentFinance.Core.Requests;
 
 public class PagedRequest : BaseRequest
 {
-  public int PageNumber { get; set; } = 1;
-  public int PageSize { get; set; } = 25;
+  public int PageNumber { get; set; } = Configuration.DefaultPageNumber;
+  public int PageSize { get; set; } = Configuration.DefaultPageSize;
 }
---
+++ FluentFinance.Core/Responses/Response.cs
@@ -0,0 +1,26 @@
+using System.Text.Json.Serialization;
+
+namespace FluentFinance.Core.Responses;
+
+public class Response<T>
+{
+  private readonly int _code;
+
+  public T? Data { get; set; }
+  public string Message { get; set; } = string.Empty;
+
+  [JsonConstructor]
+  public Response()
+  {
+    _code = Configuration.DefaultStatusCode;
+  }
+
+  public Response(T data, int code = Configuration.DefaultStatusCode, string message = null!)
+  {
+    Data = data;
+    Message = message;
+    _code = code;
+  }
+
+  [JsonIgnore] public bool IsSuccess => _code is >= 200 and <= 299;
+}
