65943a2e19a50358c166a4a74c932d0a8895f9f8
Kaique Reis <kreis1265@gmail.com>
2025-03-25 02:10:40 -0300

    feat: implementing paged search response

---
+++ FluentFinance.Core/Responses/PagedResponse.cs
@@ -0,0 +1,22 @@
+namespace FluentFinance.Core.Responses;
+
+public class PagedResponse<T> : Response<T>
+{
+  public int CurrentPage { get; set; }
+  public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+  public int PageSize { get; set; }
+  public int TotalCount { get; set; }
+
+  public PagedResponse(T data, int code = Configuration.DefaultStatusCode, string message = null!) : base(data, code,
+    message)
+  {
+  }
+
+  public PagedResponse(T data, int totalCount, int currentPage = 1, int pageSize = Configuration.DefaultPageSize) :
+    base(data)
+  {
+    TotalCount = totalCount;
+    CurrentPage = currentPage;
+    PageSize = pageSize;
+  }
+}
