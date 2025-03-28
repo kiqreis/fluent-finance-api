5bf3188a159f0ccd41fc1300e748e5ae7773d557
Kaique Reis <kreis1265@gmail.com>
2025-03-28 20:01:11 -0300

    feat: add methods interface to transaction model

---
+++ FluentFinance.Core/Handlers/ITransactionHandler.cs
@@ -0,0 +1,14 @@
+using FluentFinance.Core.Requests.Transactions;
+using FluentFinance.Core.Responses;
+using FluentFinance.Core.Responses.Transactions;
+
+namespace FluentFinance.Core.Handlers;
+
+public interface ITransactionHandler
+{
+  Task<Response<TransactionResponseDto>> CreateAsync(CreateTransactionRequest request);
+  Task<Response<TransactionResponseDto>> UpdateAsync(UpdateTransactionRequest request);
+  Task<Response<TransactionResponseDto>> DeleteAsync(long id);
+  Task<Response<TransactionResponseDto>> GetById(long id);
+  Task<Response<TransactionResponseDto>> GetByPeriod(GetTransactionByPeriodRequest request);
+}
