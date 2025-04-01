b11e749c85360823226c93eb1b3f1c91994e4b35
Kaique Reis <kreis1265@gmail.com>
2025-03-31 23:21:22 -0300

    feat: add mapping for IdentityRoleClaim table

---
+++ FluentFinance.Api/Data/Mappings/IdentityRoleClaimMapping.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FluentFinance.Api.Data.Mappings;
+
+public class IdentityRoleClaimMapping : IEntityTypeConfiguration<IdentityRoleClaim<long>>
+{
+  public void Configure(EntityTypeBuilder<IdentityRoleClaim<long>> builder)
+  {
+    builder.ToTable("IdentityRoleClaim");
+
+    builder.HasKey(x => x.Id);
+
+    builder.Property(x => x.Id)
+      .ValueGeneratedOnAdd()
+      .UseIdentityColumn();
+
+    builder.Property(x => x.ClaimType).HasMaxLength(255);
+    builder.Property(x => x.ClaimValue).HasMaxLength(255);
+  }
+}
