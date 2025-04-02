0827814d367deb0c09d400e4529399f277924aea
Kaique Reis <kreis1265@gmail.com>
2025-04-02 05:48:07 -0300

    refactor: add extension method to WebApplicationBuilder for better Program organization

---
+++ FluentFinance.Api/Common/Api/BuilderExtension.cs
@@ -0,0 +1,61 @@
+using FluentFinance.Api.Data;
+using FluentFinance.Api.Handlers;
+using FluentFinance.Api.Mappings;
+using FluentFinance.Api.Models;
+using FluentFinance.Core;
+using FluentFinance.Core.Handlers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace FluentFinance.Api.Common.Api;
+
+public static class BuilderExtension
+{
+  public static void AddConfiguration(this WebApplicationBuilder builder)
+  {
+    Configuration.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
+  }
+
+  public static void AddDocs(this WebApplicationBuilder builder)
+  {
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+  }
+
+  public static void AddSecurity(this WebApplicationBuilder builder)
+  {
+    builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme)
+      .AddIdentityCookies();
+
+    builder.Services.AddAuthorization();
+  }
+
+  public static void AddDataContexts(this WebApplicationBuilder builder)
+  {
+    builder.Services.AddDbContext<AppDbContext>(opt =>
+    {
+      opt.UseSqlServer(Configuration.ConnectionString);
+    });
+
+    builder.Services.AddIdentityCore<User>()
+      .AddRoles<IdentityRole<long>>()
+      .AddEntityFrameworkStores<AppDbContext>()
+      .AddApiEndpoints();
+  }
+
+  public static void AddCrossOrigins(this WebApplicationBuilder builder)
+  {
+    
+  }
+  
+  public static void AddServices(this WebApplicationBuilder builder)
+  {
+    builder.Services.AddScoped<ICategoryHandler, CategoryHandler>();
+    builder.Services.AddScoped<ITransactionHandler, TransactionHandler>();
+  }
+
+  public static void AddMapping(this WebApplicationBuilder builder)
+  {
+    builder.Services.AddAutoMapper(typeof(MappingProfile));
+  }
+}
--- FluentFinance.Api/Program.cs
+++ FluentFinance.Api/Program.cs
@@ -1,35 +1,14 @@
 using FluentFinance.Api.Common.Api;
-using FluentFinance.Api.Data;
-using FluentFinance.Api.Handlers;
-using FluentFinance.Api.Mappings;
-using FluentFinance.Api.Models;
-using FluentFinance.Core.Handlers;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme)
-  .AddIdentityCookies();
-builder.Services.AddAuthorization();
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
-
-builder.Services.AddDbContext<AppDbContext>(opt =>
-{
-  opt.UseSqlServer(connectionString);
-});
-
-builder.Services.AddIdentityCore<User>()
-  .AddRoles<IdentityRole<long>>()
-  .AddEntityFrameworkStores<AppDbContext>()
-  .AddApiEndpoints(); 
-
-builder.Services.AddAutoMapper(typeof(MappingProfile));
-builder.Services.AddScoped<ICategoryHandler, CategoryHandler>();
-builder.Services.AddScoped<ITransactionHandler, TransactionHandler>();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.AddConfiguration();
+builder.AddSecurity();
+builder.AddDataContexts();
+builder.AddCrossOrigins();
+builder.AddDocs();
+builder.AddMapping();
+builder.AddServices();
 
 var app = builder.Build();
 
