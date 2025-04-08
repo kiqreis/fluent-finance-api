d57c72a1446df4467813990b84669fda7191d045
Kaique Reis <kreis1265@gmail.com>
2025-04-08 10:46:00 -0300

    feat: add CookieHandler for managing authentication cookies

--- FluentFinance.Web/Program.cs
+++ FluentFinance.Web/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using FluentFinance.Web;
+using FluentFinance.Web.Security;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<CookieHandler>();
 
 builder.Services.AddMudServices();
 
