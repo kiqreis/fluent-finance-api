76b244180d3b66c4c75c0bcc4d5d0ce2f1f31d0a
Kaique Reis <kreis1265@gmail.com>
2025-04-13 18:37:41 -0300

    feat: implement logout page logic

---
+++ FluentFinance.Web/Pages/Identity/Logout.razor.cs
@@ -0,0 +1,27 @@
+﻿using FluentFinance.Core.Handlers;
+using FluentFinance.Web.Security;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace FluentFinance.Web.Pages.Identity;
+
+public partial class LogoutPage : ComponentBase
+{
+  [Inject] public ISnackbar Snackbar { get; set; } = null!;
+  [Inject] public NavigationManager NavigationManager { get; set; } = null!;
+  [Inject] public ICookieAuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
+  [Inject] public IAccountHandler Handler { get; set; } = null!;
+
+  protected override async Task OnInitializedAsync()
+  {
+    if (await AuthenticationStateProvider.CheckAuthenticatedAsync())
+    {
+      await Handler.LogoutAsync();
+      await AuthenticationStateProvider.GetAuthenticationStateAsync();
+
+      AuthenticationStateProvider.NotifyAuthenticationStateChanged();
+    }
+
+    await base.OnInitializedAsync();
+  }
+}
